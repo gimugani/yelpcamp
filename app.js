095ee7a11997d451b46cc7217080eb46c7ea4b79
Kwan Kim <kwan@mengzmom.com>
2018-09-19 19:12:46 +0000

    add price to campground

--- app.js
+++ app.js
@@ -20,7 +20,6 @@ var commentRoutes       = require('./routes/comments'),
 
      
 mongoose.connect(process.env.DATABASEURL, { useNewUrlParser: true });
-// mongoose.connect("mongodb://kwan:Hirosueq123@ds163402.mlab.com:63402/mengzmomtest", { useNewUrlParser: true });
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
