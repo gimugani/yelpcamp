7ec168644767d40b9f680d1af54df9682c8b07dc
Kwan Kim <kwan@mengzmom.com>
2018-09-19 17:59:38 +0000

    add mongolab

--- app.js
+++ app.js
@@ -19,7 +19,9 @@ var commentRoutes       = require('./routes/comments'),
     indexRoutes         = require('./routes/index');
 
      
-mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
+// mongoose.connect("mongodb://localhost/yelp_camp", { useNewUrlParser: true });
+mongoose.connect("mongodb://kwan:Hirosueq123@ds163402.mlab.com:63402/mengzmomtest", { useNewUrlParser: true });
+
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
 app.use(express.static(__dirname+"/public"));
